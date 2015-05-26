09c7cbdc8e716f75d479fe08f6ba1ba5e3bd0e5f
[angela riggs] <[riggs.ang@gmail.com]>
2015-05-26 15:59:16 -0700

    modified index.js and created post.jade

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to the Thunderdome' });
 });
 
+var postsDir =
+fs.readdir(postsDir, function(error, directoryContents) {
+  if(error) {
+    throw new Error(error);
+  }
+  directoryContents.forEach(function(postFileName) {
+    var postName = postFilename.replace('.md', '');
+    fs.readFile(postsDir + postFilename, {encoding: 'utf-8'}, function(error, fileContents) {
+      if (error) {
+        throw new Error(error);
+      }
+      var renderedPost = marked(fileContents);
+      console.log(renderedPost);
+
+      router.get('/' + postName, function(request, response) {
+        response.render('post', {postContents: renderedPost});
+      });
+    });
+  });
+});
+
 module.exports = router;
