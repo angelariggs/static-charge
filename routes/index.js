05d2217a03f40d950d32a3477c8c392d7d5361a9
[angela riggs] <[riggs.ang@gmail.com]>
2015-05-26 16:12:08 -0700

    modified index.js; created post folder and post_one file

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var marked = require('marked');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to the Thunderdome' });
+  res.render('index', { title: 'Express' });
 });
 
-var postsDir =
-fs.readdir(postsDir, function(error, directoryContents) {
+var postDir = __dirname + '/../posts/';
+fs.readdir(postDir, function(error, directoryContents) {
   if(error) {
     throw new Error(error);
   }
   directoryContents.forEach(function(postFileName) {
-    var postName = postFilename.replace('.md', '');
-    fs.readFile(postsDir + postFilename, {encoding: 'utf-8'}, function(error, fileContents) {
-      if (error) {
+    var postName = postFileName.replace('.md', '');
+    fs.readFile(postDir + postFileName, {encoding: 'utf-8'}, function(error, fileContents) {
+      if(error) {
         throw new Error(error);
       }
+
       var renderedPost = marked(fileContents);
       console.log(renderedPost);
 
       router.get('/' + postName, function(request, response) {
         response.render('post', {postContents: renderedPost});
-      });
+
+    })
     });
   });
 });
